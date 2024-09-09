bd7817927aab07240e8608682adbe1935e913a66
muItilingualism <sebhalfdan@gmail.com>
2024-09-09 20:02:23 +0200

    Add User Entity

---
+++ rest-client-quickstart/src/main/java/org/csdg8/security/jpa/User.java
@@ -0,0 +1,39 @@
+package org.csdg8.security.jpa;
+
+import java.util.Collection;
+
+import io.quarkus.elytron.security.common.BcryptUtil;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.security.jpa.Password;
+import io.quarkus.security.jpa.Roles;
+import io.quarkus.security.jpa.UserDefinition;
+import io.quarkus.security.jpa.Username;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "app-user")
+@UserDefinition
+public class User extends PanacheEntity {
+    @Username
+    public String username;
+    @Password
+    public String password;
+    @Roles
+    public Collection<String> role;
+
+    /**
+     * Adds a new user to the database
+     * 
+     * @param username the username
+     * @param password the unencrypted password (it is encrypted with bcrypt)
+     * @param role     the user assigned roles
+     */
+    public static void add(String username, String password, Collection<String> role) {
+        User user = new User();
+        user.username = username;
+        user.password = BcryptUtil.bcryptHash(password);
+        user.role = role;
+        user.persist();
+    }
+}
