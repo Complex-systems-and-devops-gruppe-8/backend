e1404f276024425ec786cd7af464b37147d9467b
muItilingualism <sebhalfdan@gmail.com>
2024-09-11 18:13:34 +0200

    Rename /auth/refresh to /auth/token/refresh

--- rest-client-quickstart/src/main/java/org/csdg8/security/AuthResource.java
+++ rest-client-quickstart/src/main/java/org/csdg8/security/AuthResource.java
@@ -79,7 +79,7 @@ public class AuthResource {
     @Operation(summary = "Refresh access token using a valid refresh token", description = "Validates the provided refresh token and, if valid, generates a new short-lived access token. The refresh token itself remains unchanged and can be used for future token refresh operations. If the refresh token is invalid, the request is rejected with an unauthorized response.")
     @APIResponse(responseCode = "200", description = "Successful token refresh, returns a new access token")
     @APIResponse(responseCode = "401", description = "Invalid refresh token")
-    @Path("/refresh")
+    @Path("/token/refresh")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response refreshToken(RefreshRequest request) {
         if (!this.refreshTokenService.isValidRefreshToken(request.username, request.refreshToken)) {
