0d10554fe300de2f33f6b9ed17cc36734eb744cc
muItilingualism <sebhalfdan@gmail.com>
2024-09-11 23:40:09 +0200

    Move /hello related tests to HelloResourceTest

--- rest-client-quickstart/src/test/java/org/csdg8/hello/HelloResourceTest.java
+++ rest-client-quickstart/src/test/java/org/csdg8/hello/HelloResourceTest.java
@@ -1,15 +1,21 @@
 package org.csdg8.hello;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 import java.net.URL;
 
+import org.apache.http.HttpStatus;
+import org.csdg8.auth.AuthResource;
+import org.csdg8.auth.AuthResource.CreateTokenResponse;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.Response;
 
 @QuarkusTest
 public class HelloResourceTest {
@@ -18,6 +24,9 @@ public class HelloResourceTest {
     @TestHTTPResource
     URL url;
 
+    @Inject
+    AuthResource authResource;
+
     @Test
     public void shouldReturnHelloWorldWhenAccessingAllEndpoint() {
         given()
@@ -25,5 +34,79 @@ public class HelloResourceTest {
                 .then()
                 .statusCode(200)
                 .body(is("Hello, World!"));
+    }
+
+    @Test
+    void shouldAccessPublicWhenAnonymous() {
+        get(this.url + "/all")
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    void shouldNotAccessAdminWhenAnonymous() {
+        get(this.url + "/admin")
+                .then()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED);
+    }
+
+    @Test
+    void shouldAccessAdminWhenAdminAuthenticated() {
+        String adminToken = obtainToken("admin", "admin");
+
+        given().header("Authorization", "Bearer " + adminToken)
+                .when()
+                .get(this.url + "/admin")
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    void shouldAccessAllWhenAdminAuthenticated() {
+        String adminToken = obtainToken("admin", "admin");
+
+        given().header("Authorization", "Bearer " + adminToken)
+                .when()
+                .get(this.url + "/all")
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    void shouldNotAccessUserWhenAdminAuthenticated() {
+        String adminToken = obtainToken("admin", "admin");
+
+        given().header("Authorization", "Bearer " + adminToken)
+                .when()
+                .get(this.url + "/user")
+                .then()
+                .statusCode(HttpStatus.SC_FORBIDDEN);
+    }
+
+    @Test
+    void shouldAccessUserAndGetIdentityWhenUserAuthenticated() {
+        String username = "user";
+        String userToken = obtainToken(username, "user");
+
+        given().header("Authorization", "Bearer " + userToken)
+                .when()
+                .get(this.url + "/user")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body(is("Hello, " + username + "!"));
+    }
+
+    private String obtainToken(String username, String password) {
+        Response response = authResource.createToken(new AuthResource.Credentials(username, password));
+        CreateTokenResponse tokenResponse = response.readEntity(CreateTokenResponse.class);
+        if (response.getStatus() != HttpStatus.SC_OK) {
+            throw new RuntimeException(String.format(
+                    "Failed to obtain token for user: %s"
+                            + " with password: %s"
+                            + ". Response: %s",
+                    username, password, response.readEntity(String.class)));
+        }
+
+        return tokenResponse.accessToken;
     }
 }
--- rest-client-quickstart/src/test/java/org/csdg8/security/jpa/JpaSecurityRealmTest.java
+++ rest-client-quickstart/src/test/java/org/csdg8/security/jpa/JpaSecurityRealmTest.java
@@ -1,15 +1,11 @@
 package org.csdg8.security.jpa;
 
-import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.Is.is;
 
 import java.net.URL;
 
 import org.apache.http.HttpStatus;
-import org.csdg8.auth.AuthResource;
-import org.csdg8.auth.AuthResource.CreateTokenResponse;
-import org.csdg8.hello.HelloResource;
 import org.csdg8.user.UserResource;
 import org.csdg8.user.UserResource.RegistrationRequest;
 import org.junit.jupiter.api.Test;
@@ -18,82 +14,13 @@ import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.core.Response;
 
 @QuarkusTest
 public class JpaSecurityRealmTest {
 
-    @TestHTTPEndpoint(HelloResource.class)
-    @TestHTTPResource
-    URL helloUrl;
-
     @TestHTTPEndpoint(UserResource.class)
     @TestHTTPResource
     URL usersUrl;
-
-    @Inject
-    AuthResource authResource;
-
-    @Test
-    void shouldAccessPublicWhenAnonymous() {
-        get(this.helloUrl + "/all")
-                .then()
-                .statusCode(HttpStatus.SC_OK);
-    }
-
-    @Test
-    void shouldNotAccessAdminWhenAnonymous() {
-        get(this.helloUrl + "/admin")
-                .then()
-                .statusCode(HttpStatus.SC_UNAUTHORIZED);
-    }
-
-    @Test
-    void shouldAccessAdminWhenAdminAuthenticated() {
-        String adminToken = obtainToken("admin", "admin");
-
-        given().header("Authorization", "Bearer " + adminToken)
-                .when()
-                .get(this.helloUrl + "/admin")
-                .then()
-                .statusCode(HttpStatus.SC_OK);
-    }
-
-    @Test
-    void shouldAccessAllWhenAdminAuthenticated() {
-        String adminToken = obtainToken("admin", "admin");
-
-        given().header("Authorization", "Bearer " + adminToken)
-                .when()
-                .get(this.helloUrl + "/all")
-                .then()
-                .statusCode(HttpStatus.SC_OK);
-    }
-
-    @Test
-    void shouldNotAccessUserWhenAdminAuthenticated() {
-        String adminToken = obtainToken("admin", "admin");
-
-        given().header("Authorization", "Bearer " + adminToken)
-                .when()
-                .get(this.helloUrl + "/user")
-                .then()
-                .statusCode(HttpStatus.SC_FORBIDDEN);
-    }
-
-    @Test
-    void shouldAccessUserAndGetIdentityWhenUserAuthenticated() {
-        String username = "user";
-        String userToken = obtainToken(username, "user");
-
-        given().header("Authorization", "Bearer " + userToken)
-                .when()
-                .get(this.helloUrl + "/user")
-                .then()
-                .statusCode(HttpStatus.SC_OK)
-                .body(is("Hello, " + username + "!"));
-    }
 
     @Test
     public void testSuccessfulUserRegistration() {
@@ -155,17 +82,4 @@ public class JpaSecurityRealmTest {
                 .statusCode(400);
     }
 
-    private String obtainToken(String username, String password) {
-        Response response = authResource.createToken(new AuthResource.Credentials(username, password));
-        CreateTokenResponse tokenResponse = response.readEntity(CreateTokenResponse.class);
-        if (response.getStatus() != HttpStatus.SC_OK) {
-            throw new RuntimeException(String.format(
-                    "Failed to obtain token for user: %s"
-                            + " with password: %s"
-                            + ". Response: %s",
-                    username, password, response.readEntity(String.class)));
-        }
-
-        return tokenResponse.accessToken;
-    }
 }
