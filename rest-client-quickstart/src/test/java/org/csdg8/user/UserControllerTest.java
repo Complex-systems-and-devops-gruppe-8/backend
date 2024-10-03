becef30f5a49e7fe6a0af9e35e74860b17f3fb6c
muItilingualism <sebhalfdan@gmail.com>
2024-10-03 12:35:22 +0200

    Add controller tests
    The responsibility of the controller is to unwrap the HTTP requests and call the service and map the domain results to HTTP responses.
    
    The tests should reflect this responsibility.

---
+++ rest-client-quickstart/src/test/java/org/csdg8/user/UserControllerTest.java
@@ -0,0 +1,83 @@
+package org.csdg8.user;
+
+import io.quarkus.test.TestTransaction;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.Response;
+
+import org.csdg8.model.exception.InvalidCredentialsException;
+import org.csdg8.model.exception.UserAlreadyExistsException;
+import org.csdg8.model.exception.UserNotFoundException;
+import org.csdg8.user.dto.RegistrationRequest;
+import org.csdg8.user.dto.UserResponse;
+import org.csdg8.user.dto.UserResponseList;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
+@QuarkusTest
+public class UserControllerTest {
+    
+    @Inject
+    UserController userController;
+
+    @Test
+    @TestTransaction
+    public void shouldRegisterUserAndReturnCreatedResponse() {
+        RegistrationRequest request = new RegistrationRequest("john", "password123");
+        Response response = userController.register(request);
+
+        assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
+        assertEquals("User registered successfully", response.getEntity());
+    }
+
+    @Test
+    public void shouldThrowUserAlreadyExistsExceptionWhenRegisteringExistingUser() {
+        RegistrationRequest request = new RegistrationRequest("admin", "password123");
+        assertThrowsExactly(UserAlreadyExistsException.class,() -> {
+            userController.register(request);
+        });
+    }
+
+    @Test
+    public void shouldThrowInvalidCredentialsExceptionForInvalidCredentials() {
+        RegistrationRequest request = new RegistrationRequest("u", "short");
+        assertThrowsExactly(InvalidCredentialsException.class, () -> {
+            userController.register(request);
+        });
+    }
+
+    @Test
+    public void shouldReturnAllUsers() {
+        Response response = userController.all();
+
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+        UserResponseList userResponseList = (UserResponseList) response.getEntity();
+        assertNotNull(userResponseList);
+        List<UserResponse> users = userResponseList.users;
+        assertEquals(2, users.size());
+        assertEquals("admin", users.get(0).username);
+        assertEquals("user", users.get(1).username);
+    }
+
+    @Test
+    public void shouldReturnUserWhenGettingExistingUser() {
+        Response response = userController.one("admin");
+
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+        UserResponse userResponse = (UserResponse) response.getEntity();
+        assertNotNull(userResponse);
+        assertEquals("admin", userResponse.username);
+    }
+
+    @Test
+    public void shouldThrowUserNotFoundExceptionWhenGettingNonExistentUser() {
+        assertThrowsExactly(UserNotFoundException.class, () -> {
+            this.userController.one("nonexistentuser");
+        });
+    }
+}
