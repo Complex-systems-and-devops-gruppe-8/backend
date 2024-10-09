20e8e0c2330a3e79f08a73731e3a1022f0af012e
muItilingualism <sebhalfdan@gmail.com>
2024-10-09 12:08:36 +0200

    Re-add startup dev profiles
    Note: these profiles will be purged if the tests are run!

---
+++ rest-client-quickstart/src/main/java/org/csdg8/infrastructure/Startup.java
@@ -0,0 +1,23 @@
+package org.csdg8.infrastructure;
+
+import java.util.Set;
+
+import org.csdg8.user.User;
+
+import io.quarkus.arc.profile.IfBuildProfile;
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Singleton;
+import jakarta.transaction.Transactional;
+
+@Singleton
+public class Startup {
+
+    @Transactional
+    @IfBuildProfile(allOf = { "dev" })
+    public void loadDevUsers(@Observes StartupEvent evt) {
+        User.deleteAll();
+        User.add("admin", "admin", Set.of("admin"));
+        User.add("user", "user", Set.of("user"));
+    }
+}
