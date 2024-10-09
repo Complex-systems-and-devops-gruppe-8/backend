95bdc4389109e17861bb323b5de3db5369de6489
muItilingualism <sebhalfdan@gmail.com>
2024-10-09 17:58:13 +0200

    Add missing XMLElement annotations
    Allows for richer swagger api spec

--- rest-client-quickstart/src/main/java/org/csdg8/auth/dto/CreateTokenRequest.java
+++ rest-client-quickstart/src/main/java/org/csdg8/auth/dto/CreateTokenRequest.java
@@ -1,16 +1,18 @@
 package org.csdg8.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.xml.bind.annotation.XmlElement;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateTokenRequest {
-
     @NotBlank
+    @XmlElement
     public String username;
 
     @NotBlank
+    @XmlElement
     public String password;
 }
--- rest-client-quickstart/src/main/java/org/csdg8/auth/dto/RefreshAccessTokenRequest.java
+++ rest-client-quickstart/src/main/java/org/csdg8/auth/dto/RefreshAccessTokenRequest.java
@@ -1,16 +1,18 @@
 package org.csdg8.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.xml.bind.annotation.XmlElement;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 public class RefreshAccessTokenRequest {
-    
     @NotBlank
+    @XmlElement
     public String refreshToken;
 
     @NotBlank
+    @XmlElement
     public String accessToken;
 }
