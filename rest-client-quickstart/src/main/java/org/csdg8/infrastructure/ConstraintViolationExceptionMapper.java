cf7aad6b54528601f26efc402a453bf3b7448bd4
muItilingualism <sebhalfdan@gmail.com>
2024-10-10 14:48:01 +0200

    Format code

--- rest-client-quickstart/src/main/java/org/csdg8/infrastructure/ConstraintViolationExceptionMapper.java
+++ rest-client-quickstart/src/main/java/org/csdg8/infrastructure/ConstraintViolationExceptionMapper.java
@@ -1,4 +1,3 @@
-
 package org.csdg8.infrastructure;
 
 import io.vertx.core.json.JsonArray;
@@ -10,7 +9,8 @@ import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
-/**<p>
+/**
+ * <p>
  * Courtesy of gwenneg
  * {@link https://github.com/gwenneg/notifications-backend/blob/93deedc5345fc60c289706ab4ea4e418de2b2235/backend/src/main/java/com/redhat/cloud/notifications/routers/exception/ConstraintViolationExceptionMapper.java}
  * </p>
@@ -22,21 +22,27 @@ import jakarta.ws.rs.ext.Provider;
  * <p>
  * Original javadoc:
  * "Overrides the default exception mapper for the {@link ConstraintViolation}
-    exceptions. The reason is that if any method which isn't a top level handler
-    gets annotated with a {@link jakarta.validation.Valid} annotation, and the
-    incoming object is not valid, for some reason instead of capturing that
-    {@link ConstraintViolation} and returning a {@link jakarta.ws.rs.BadRequestException},
-    an {@link jakarta.ws.rs.InternalServerErrorException} is returned with a
-    {@link HttpStatus#SC_INTERNAL_SERVER_ERROR} status code. This exception
-    mapper avoids that and returns the proper response for any {@link ConstraintViolation}
-    that gets thrown in the code."
+ * exceptions. The reason is that if any method which isn't a top level handler
+ * gets annotated with a {@link jakarta.validation.Valid} annotation, and the
+ * incoming object is not valid, for some reason instead of capturing that
+ * {@link ConstraintViolation} and returning a
+ * {@link jakarta.ws.rs.BadRequestException},
+ * an {@link jakarta.ws.rs.InternalServerErrorException} is returned with a
+ * {@link HttpStatus#SC_INTERNAL_SERVER_ERROR} status code. This exception
+ * mapper avoids that and returns the proper response for any
+ * {@link ConstraintViolation}
+ * that gets thrown in the code."
  * </p>
  * 
  * <p>
- * To use this functionality, annotate request class fields with {@link jakarta.validation.constraints.NotNull}, etc. 
- * and when these properties must hold (for example in a controller class) annotate the parameter with {@link jakarta.validation.Valid}.
- * In the case that the constraint-violations do not hold a {@link ConstraintViolationException} is thrown, caught here 
- * and a response is returned outlining all the fields which violated the requirements.
+ * To use this functionality, annotate request class fields with
+ * {@link jakarta.validation.constraints.NotNull}, etc.
+ * and when these properties must hold (for example in a controller class)
+ * annotate the parameter with {@link jakarta.validation.Valid}.
+ * In the case that the constraint-violations do not hold a
+ * {@link ConstraintViolationException} is thrown, caught here
+ * and a response is returned outlining all the fields which violated the
+ * requirements.
  * </p>
  * 
  * <pre>
@@ -65,7 +71,7 @@ import jakarta.ws.rs.ext.Provider;
  * </pre>
  */
 @Provider
-public class ConstraintViolationExceptionMapper implements ExceptionMapper<ConstraintViolationException>{
+public class ConstraintViolationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
     @Override
     public Response toResponse(final ConstraintViolationException e) {
         final JsonObject responseBody = new JsonObject();
@@ -89,9 +95,9 @@ public class ConstraintViolationExceptionMapper implements ExceptionMapper<Const
         responseBody.put("violations", violations);
 
         return Response
-            .status(Response.Status.BAD_REQUEST)
-            .type(MediaType.APPLICATION_JSON)
-            .entity(responseBody.encode())
-            .build();
+                .status(Response.Status.BAD_REQUEST)
+                .type(MediaType.APPLICATION_JSON)
+                .entity(responseBody.encode())
+                .build();
     }
 }
