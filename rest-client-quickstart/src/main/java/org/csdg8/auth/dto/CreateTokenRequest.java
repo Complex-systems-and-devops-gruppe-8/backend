2f2842c47e59207c0e16e578ab1d4a77eed9b73a
muItilingualism <sebhalfdan@gmail.com>
2024-10-10 21:39:32 +0200

    Add username and password request constraints

--- rest-client-quickstart/src/main/java/org/csdg8/auth/dto/CreateTokenRequest.java
+++ rest-client-quickstart/src/main/java/org/csdg8/auth/dto/CreateTokenRequest.java
@@ -1,6 +1,7 @@
 package org.csdg8.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import jakarta.xml.bind.annotation.XmlElement;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreateTokenRequest {
     @NotBlank
+    @Size(min = 3, max = 30)
     @XmlElement
     public String username;
 
     @NotBlank
+    @Size(min=8, max=50)
     @XmlElement
     public String password;
 }
--- rest-client-quickstart/src/main/java/org/csdg8/user/dto/RegistrationRequest.java
+++ rest-client-quickstart/src/main/java/org/csdg8/user/dto/RegistrationRequest.java
@@ -1,20 +1,24 @@
 package org.csdg8.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-@XmlRootElement
 @AllArgsConstructor
 @NoArgsConstructor
+@XmlRootElement
 public class RegistrationRequest {
-    @XmlElement
+    
     @NotBlank
+    @Size(min = 3, max = 30)
+    @XmlElement
     public String username;
     
-    @XmlElement
     @NotBlank
+    @Size(min = 8, max = 50)
+    @XmlElement
     public String password;
 }
