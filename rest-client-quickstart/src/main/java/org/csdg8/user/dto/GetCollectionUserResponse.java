6df4d789c1b59dd0879711a09cdbfb6d12142162
muItilingualism <sebhalfdan@gmail.com>
2024-10-14 12:30:33 +0200

    Add create-user action to /users

--- rest-client-quickstart/src/main/java/org/csdg8/user/dto/GetCollectionUserResponse.java
+++ rest-client-quickstart/src/main/java/org/csdg8/user/dto/GetCollectionUserResponse.java
@@ -2,10 +2,20 @@ package org.csdg8.user.dto;
 
 import java.util.Collection;
 
+import com.google.code.siren4j.annotations.Siren4JAction;
+import com.google.code.siren4j.annotations.Siren4JActionField;
 import com.google.code.siren4j.annotations.Siren4JEntity;
+import com.google.code.siren4j.component.impl.ActionImpl.Method;
 import com.google.code.siren4j.resource.CollectionResource;
 
-@Siren4JEntity(name = "users", uri = "/users")
+import jakarta.ws.rs.core.MediaType;
+
+@Siren4JEntity(name = "users", uri = "/users", actions = {
+        @Siren4JAction(name = "create-user", title = "Create user", method = Method.POST, href = "/users", type = MediaType.APPLICATION_JSON, fields = {
+                @Siren4JActionField(name = "username", title = "Username", required = true, type = "text"),
+                @Siren4JActionField(name = "password", title = "Plain-text password", required = true, type = "text")
+        })
+})
 public class GetCollectionUserResponse extends CollectionResource<GetUserResponse> {
     public GetCollectionUserResponse(Collection<GetUserResponse> items) {
         setItems(items);
