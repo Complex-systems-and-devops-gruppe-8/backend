e3bffdd20fc94cbe8f6b578a78eadc14d102715c
muItilingualism <sebhalfdan@gmail.com>
2024-10-19 21:48:08 +0200

    Add explicit produces siren json annotation

--- rest-client-quickstart/src/main/java/org/csdg8/root/RootResource.java
+++ rest-client-quickstart/src/main/java/org/csdg8/root/RootResource.java
@@ -3,14 +3,17 @@ package org.csdg8.root;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
+import com.google.code.siren4j.Siren4J;
 import com.google.code.siren4j.component.Entity;
 import com.google.code.siren4j.error.Siren4JException;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 
 @Path("/")
+@Produces(Siren4J.JSON_MEDIATYPE)
 public class RootResource {
 
     @Inject
