df22e383b21567bbd742caeef3b597f5ab0c1fd8
muItilingualism <sebhalfdan@gmail.com>
2024-10-19 23:48:23 +0200

    Move siren JSON assertion helper methods to utility SirenAssertion class

--- rest-client-quickstart/src/test/java/org/csdg8/auth/AuthResourceTest.java
+++ rest-client-quickstart/src/test/java/org/csdg8/auth/AuthResourceTest.java
@@ -1,7 +1,10 @@
 package org.csdg8.auth;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.csdg8.util.SirenAssertion.itShouldHaveField;
+import static org.csdg8.util.SirenAssertion.itShouldHaveHref;
+import static org.csdg8.util.SirenAssertion.itShouldHaveJSONAcceptType;
+import static org.csdg8.util.SirenAssertion.itShouldHaveMethod;
 
 import java.lang.reflect.Type;
 import java.net.URL;
@@ -33,7 +36,6 @@ import io.restassured.path.json.mapper.factory.Jackson2ObjectMapperFactory;
 import io.restassured.response.Response;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.core.MediaType;
 import lombok.SneakyThrows;
 
 @QuarkusTest
@@ -211,74 +213,6 @@ public class AuthResourceTest {
         itShouldHaveHref(response, actionName, "/auth/token/refresh");
         itShouldHaveField(response, actionName, "accessToken", "TEXT", true);
         itShouldHaveField(response, actionName, "refreshToken", "TEXT", true);
-    }
-
-    /**
-     * Parses the given {@code response} JSON, finds the action by
-     * {@code actionName}
-     * and asserts it contains a method field equal to the supplied
-     * {@code methodType}.
-     * 
-     * @param response
-     * @param actionName
-     * @param methodType
-     */
-    private static void itShouldHaveMethod(Response response, String actionName, Method methodType) {
-        response.then()
-                .body("actions.find { it.name == '" + actionName + "' }.method",
-                        equalTo(methodType.toString()));
-    }
-
-    /**
-     * Parses the given {@code response} JSON, finds the action by
-     * {@code actionName}
-     * and asserts it contains a type field equal to the {@code application/json}.
-     * 
-     * @param response
-     * @param actionName
-     */
-    private static void itShouldHaveJSONAcceptType(Response response, String actionName) {
-        response.then()
-                .body("actions.find { it.name == '" + actionName + "' }.type",
-                        equalTo(MediaType.APPLICATION_JSON.toString()));
-    }
-
-    /**
-     * Parses the given {@code response} JSON, finds the action by
-     * {@code actionName}
-     * and asserts it contains the supplied {@code href}.
-     * 
-     * @param response   the response body to parse
-     * @param actionName the name of the action
-     * @param href       the expected href uri
-     */
-    private static void itShouldHaveHref(Response response, String actionName, String href) {
-        response.then()
-                .body("actions.find { it.name == '" + actionName + "' }.href",
-                        equalTo(href));
-    }
-
-    /**
-     * Parses the given {@code response} JSON, finds the action by
-     * {@code actionName}
-     * and asserts it has the supplied {@code fieldName}, {@code type},
-     * and whether the field {@code isRequired}.
-     * 
-     * @param response   the response body to parse
-     * @param actionName the name of the action
-     * @param fieldName  the name of the expected field in the action
-     * @param type       the type of the field in the expected field
-     * @param isRequired whether the field is expected to be required.
-     */
-    private static void itShouldHaveField(Response response, String actionName, String fieldName, String type,
-            boolean isRequired) {
-        response.then()
-                .body("actions.find { it.name == '" + actionName + "' }.fields.find { it.name == '" + fieldName
-                        + "' }.required",
-                        equalTo(isRequired))
-                .body("actions.find { it.name == '" + actionName + "' }.fields.find { it.name == '" + fieldName
-                        + "' }.type",
-                        equalTo(type));
     }
 
     @SneakyThrows
