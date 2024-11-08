9e4d3af0892576c1800e1eedb076e83a29a31c7d
muItilingualism <sebhalfdan@gmail.com>
2024-11-08 18:20:02 +0100

    Add min and digits constraint to beAmount

--- rest-client-quickstart/src/main/java/org/csdg8/game/coinflip/dto/PlayCoinFlipRequest.java
+++ rest-client-quickstart/src/main/java/org/csdg8/game/coinflip/dto/PlayCoinFlipRequest.java
@@ -2,6 +2,8 @@ package org.csdg8.game.coinflip.dto;
 
 import org.csdg8.game.coinflip.model.CoinFlipState;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,5 +19,7 @@ public class PlayCoinFlipRequest {
     private CoinFlipState choice;
 
     @NotNull
+    @Min(value = 1)
+    @Digits(integer = 99, fraction = 0)
     private Long betAmount;
 }
