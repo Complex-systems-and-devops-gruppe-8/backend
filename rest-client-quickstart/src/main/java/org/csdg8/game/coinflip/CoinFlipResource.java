d41f32f82115a60572d1f6f3964b663eed6e8d07
muItilingualism <sebhalfdan@gmail.com>
2024-11-08 18:22:21 +0100

    Remove unused imports

--- rest-client-quickstart/src/main/java/org/csdg8/game/coinflip/CoinFlipResource.java
+++ rest-client-quickstart/src/main/java/org/csdg8/game/coinflip/CoinFlipResource.java
@@ -9,17 +9,13 @@ import com.google.code.siren4j.Siren4J;
 import com.google.code.siren4j.component.Entity;
 import com.google.code.siren4j.error.Siren4JException;
 
-import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.Context;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.SecurityContext;
 
 @Path("/game/coin-flip")
 @Produces(Siren4J.JSON_MEDIATYPE)
