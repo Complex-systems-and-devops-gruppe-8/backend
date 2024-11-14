e9b01abe6b4bdcb868b0c71556125f9e8534e105
muItilingualism <sebhalfdan@gmail.com>
2024-11-14 17:49:27 +0100

    Change actionShouldHaveType to require explicit type

--- rest-client-quickstart/src/test/java/org/csdg8/util/SirenAssertion.java
+++ rest-client-quickstart/src/test/java/org/csdg8/util/SirenAssertion.java
@@ -5,10 +5,10 @@ import static org.hamcrest.Matchers.equalTo;
 import com.google.code.siren4j.component.impl.ActionImpl.Method;
 
 import io.restassured.response.Response;
-import jakarta.ws.rs.core.MediaType;
 
 /**
- * Utility class to assert properties of Siren JSON RestAssured {@link Response} bodies.
+ * Utility class to assert properties of Siren JSON RestAssured {@link Response}
+ * bodies.
  */
 public class SirenAssertion {
     /**
@@ -30,15 +30,16 @@ public class SirenAssertion {
     /**
      * Parses the given {@code response} JSON, finds the action by
      * {@code actionName}
-     * and asserts it contains a type field equal to the {@code application/json}.
+     * and asserts it contains a type field equal to the {@code type}.
      * 
      * @param response
      * @param actionName
+     * @param type
      */
-    public static void actionShouldHaveType(Response response, String actionName) {
+    public static void actionShouldHaveType(Response response, String actionName, String type) {
         response.then()
                 .body("actions.find { it.name == '" + actionName + "' }.type",
-                        equalTo(MediaType.APPLICATION_JSON.toString()));
+                        equalTo(type.toString()));
     }
 
     /**
